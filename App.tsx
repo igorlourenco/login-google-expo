3bc746aeb0d1fee2cb67b1ebcd89bbae6bf5f8c0
Igor Lourenço <theigorlourenco@gmail.com>
2020-12-09 15:49:38 -0300

    Set Up Login and Logout

--- App.tsx
+++ App.tsx
@@ -1,36 +1,79 @@
-import {StatusBar} from 'expo-status-bar';
-import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Alert, Button, StyleSheet, Text, View} from 'react-native';
 import * as Google from 'expo-google-app-auth';
 import {GoogleLogInConfig} from "expo-google-app-auth";
+import * as Updates from "expo-updates"
 
 export default function App() {
 
+    useEffect(() => {
+        async function updateApp() {
+            const { isAvailable } = await Updates.checkForUpdateAsync();
+            if (isAvailable) {
+                await Updates.fetchUpdateAsync();
+                await Updates.reloadAsync(); // depende da sua estratégia
+            }
+        }
+        updateApp();
+    }, [])
+
+    const [user, setUser] = useState(null)
+    const [accessToken, setAccessToken] = useState(null)
+    const [e, setE] = useState(null)
+
+    const config: GoogleLogInConfig = {
+        androidClientId: "", // expo
+        androidStandaloneAppClientId: "", // production
+    }
 
     async function handleSignIn() {
+        try {
+            const {type, user, accessToken} = await Google.logInAsync(config);
 
-        const config: GoogleLogInConfig = {
-            androidClientId: "INFORME AQUI O ANDROID CLIENT ID",
+            if (type === 'success') {
+                setUser(user)
+                setAccessToken(accessToken)
+                return accessToken
+            } else {
+                return {cancelled: true}
+            }
+        }catch (e) {
+            setE(e.toString())
+            return { error: true }
         }
+    }
 
-        const { type, user } = await Google.logInAsync(config);
-
-        if (type === 'success') {
-            console.log(`Logado como: ${user.name}`);
+    async function handleSignOut() {
+        try {
+            await Google.logOutAsync({ accessToken, ...config })
+            setAccessToken(null)
+            console.log('deslogado')
+        }catch (e) {
+            console.log(e)
+            return { error: true }
         }
     }
 
 
-    return (
-        <View style={styles.container}>
-            <Button onPress={handleSignIn} title="LOGIN"/>
-        </View>
-    );
+    if (user && accessToken)
+        return (
+            <View style={styles.container}>
+                <Text>{user.name}</Text>
+                <Button onPress={handleSignOut} title="LOG OUT"/>
+            </View>
+        )
+    else
+        return (
+            <View style={styles.container}>
+                <Text>{e}</Text>
+                <Button onPress={handleSignIn} title="LOG IN"/>
+            </View>
+        )
 }
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#aaa',
         alignItems: 'center',
         justifyContent: 'center',
     },
